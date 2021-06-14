2c8132b7c9a206b3d38f1dc74bd162d98ddc8856
Daniel Thurau <daniel.n.thurau@gmail.com>
2021-06-13 18:26:31 -0700

    Node js file that will request the img, load the img into a buffer, and upload the buffer to S3 without saving any files to the FS. This is in preparation for the lambda runtime env

--- photoRetrieval.js
+++ photoRetrieval.js
@@ -1,37 +1,81 @@
 import nodeFetch from 'node-fetch';
 import { createApi } from 'unsplash-js';
-import { createWriteStream, existsSync, readFileSync} from 'fs';
+import { readFileSync } from 'fs';
 import request from 'request';
+import AWS from 'aws-sdk';
+
+
 
 const env = JSON.parse(readFileSync('./.env.json', 'utf-8'))
 
-const unsplash = createApi({
-    accessKey: env.unsplash.access_key,
-    fetch: nodeFetch,
-});
 
-var download = function(uri, filename, callback) {
-  request.head(uri, function(err, res, body){
-    request(uri).pipe(createWriteStream(filename)).on('close', callback);
+
+
+
+function uploadToS3(uri, s3, key) {
+
+  console.log("Attempting to upload photo at " + uri + " to " + key);
+
+  var options = {
+    uri: uri,
+    encoding: null
+  };
+
+  var params = {
+    Bucket: env.aws.s3_bucket_name,
+    Key: key
+  };
+
+  request(options, function(error, response, body) {
+      if (error || response.statusCode !== 200) { 
+          console.log("Failed to get image at " + options.uri);
+          console.log(error);
+      } else {
+        s3.headObject(params, function (err, metadata) {  
+          if (err && err.code === 'NotFound') {  
+            s3.putObject({
+              Body: body,
+              Key: key.toString(),
+              Bucket: env.aws.s3_bucket_name
+            }, function(error, data) { 
+              if (error) {
+                console.log("Error uploading image to s3: " + key);
+                console.log(error);
+              } else {
+                console.log("Success uploading to s3: " + key );
+              }
+            }); 
+          } else {
+            console.log("File exists in bucket in s3, not uploading " + key);
+          }
+        });          
+      }   
   });
-};
+}
+
+const REGION = env.aws.s3_region;
+AWS.config.update({region: REGION});
+const s3 = new AWS.S3({apiVersion: '2006-03-01'});
+
+const unsplash = createApi({
+  accessKey: env.unsplash.access_key,
+  fetch: nodeFetch,
+});
 
 unsplash.photos.list({}).then(result => {
   if (result.errors) {
-    console.log('error occurred: ', result.errors[0]);
+    console.log('Error occurred when listing photos from unsplash: ', result.errors[0]);
   } else {
-    console.log(`Request status code: ${result.status}`);
+    console.log(`Request status code from listing photot: ${result.status}`);
     
     const photos = result.response.results;
     photos.forEach(photo => {
-      var filePath = './images/' + photo.id +'.jpg';
-      if (!existsSync(filePath)) {
-        download(photo.urls.raw, filePath, function(){
-          console.log(`Downloaded file to ${filePath}`);
-        });
-      } else {
-        console.log("Image has already been downloaded");
-      }
+
+      const url = photo.urls.raw;
+      const key = photo.id + ".jpg";
+
+      uploadToS3(url, s3, key);
+
     });
   }
 });
